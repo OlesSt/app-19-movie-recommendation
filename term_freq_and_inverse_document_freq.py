9c04aba8965cf344896a2311fe657e558c755ae6
Oles <oles1stepanov@gmail.com>
2023-06-22 19:24:08 +0300

    Define similar movies by similar matrix

--- term_freq_and_inverse_document_freq.py
+++ term_freq_and_inverse_document_freq.py
@@ -17,3 +17,14 @@ similarity_matrix = linear_kernel(tfidf_matrix, tfidf_matrix)
 print(similarity_matrix)
 
 
+def similar_movie(movie_title, nr_movie):
+    idx = movies_db.loc[movies_db['title'] == movie_title].index[0]
+    scores = list(enumerate(similarity_matrix[idx]))
+    scores = sorted(scores, key=lambda x: x[1], reverse=True)
+    movies_indices = [tpl[0] for tpl in scores[1:nr_movie+1]]
+    similar_titles = list(movies_db['title'].iloc[movies_indices])
+    return similar_titles
+
+
+print(similar_movie('Kung Fu Panda 3', 3))
+
